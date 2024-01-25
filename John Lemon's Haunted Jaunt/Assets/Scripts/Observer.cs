98dde84607299f120b8a35a014aa8a36996f56f0
kalbii <mathieuwhang587@riversideunified.org>
2024-01-25 13:43:44 -0800

    Update

--- John Lemon's Haunted Jaunt/Assets/Scripts/Observer.cs
+++ John Lemon's Haunted Jaunt/Assets/Scripts/Observer.cs
@@ -29,7 +29,7 @@ public class Observer : MonoBehaviour
             {
                 if(raycastHit.collider.transform == player)
                 {
-
+                    gameEnding.CaughtPlayer ();
                 }
             }
         }
